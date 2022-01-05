96896f87f8a41dc3705817e9c48f0675de5c4e07
Nataliia-Ml <nata.melnik1998@gmail.com>
2022-01-05 18:16:36 +0200

    hello_world.py was change

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,19 @@
-print("hello world")
+# print("hello world")
+# print(True)
+# true=True
+# print(true)
+#
+# #false = 0, '', None
+# print(bool(0))
+# print(bool({}))
+# print(bool(None))
+# print(bool([]))
+# #тру - все остальное
+#
+# print(bool(1*(10)**(-323)))
+# print(type(1*(10)**(-32)))
+#
+# print(True or False)
+# print(True or True)
+# print(False or False)
+
